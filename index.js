a3e8a42347de50d118e27a52bf8b87349f57cc42
mariegrace31 <mariegracebmg@gmail.com>
2023-06-14 17:24:06 +0200

    Adding the date and time

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import EventListener from './modules/event.js';
 import renderBookList from './modules/booklist.js';
 import addBook from './modules/add.js';
+import { DateTime } from './node_modules/luxon/src/luxon.js';
+
+
+setInterval(() => {
+    const date = DateTime.now();
+    document.querySelector('#date').innerHTML = date.toLocaleString(
+      DateTime.DATETIME_MED,
+    );
+  }, 1000);
 
 EventListener();
 renderBookList();
